515754351b0cbf1683d2f8cfbc1ebfa77313c764
Mark Helt <heltmm@gmail.com>
2017-09-15 16:09:20 -0700

    add capybara specs

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,14 +1,68 @@
-# require('capybara/rspec')
-# require('./app')
-#
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('You have', {:type => :feature}) do
-#   it('add a word to the list') do
-#     visit('/')
-#     fill_in('name', :with => 'Apple')
-#     click_button('✓Add a Word!')
-#     expect(page).to have_content('Apple')
-#   end
-# end
+require('capybara/rspec')
+require('./app')
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('You have', {:type => :feature}) do
+  before() do
+    Word.clear()
+  end
+  it('add a word to the list') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    expect(page).to have_content('Apple')
+  end
+  it('click link of word added to list') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    click_link('Apple')
+    expect(page).to have_content('Apple')
+  end
+  it('add definition to a word page') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    click_link('Apple')
+    expect(page).to have_content('Apple')
+    fill_in('definition', :with => 'tasty fruit')
+    click_button('Add Definition!')
+    expect(page).to have_content('Definition 1: tasty fruit')
+  end
+  it('add an image to a word page') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    click_link('Apple')
+    expect(page).to have_content('Apple')
+    fill_in('link', :with => 'https://www.thesun.co.uk/wp-content/uploads/2017/01/gettyimages-185071735.jpg?strip=all&w=960')
+    click_button('Add an Image!')
+    expect(page).to have_xpath("//img[contains(@src,'https://www.thesun.co.uk/wp-content/uploads/2017/01/gettyimages-185071735.jpg?strip=all&w=960')]")
+  end
+  it('add definition to a word page') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    click_link('Apple')
+    click_button('Delete')
+    expect(page).to have_no_content('Apple')
+  end
+  it('see all words and definitions') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    fill_in('name', :with => 'Tree')
+    click_button('✓Add a Word!')
+    click_button('See all Words and Definitions')
+    expect(page).to have_content('Apple No Definition added yet Tree No Definition added yet')
+  end
+  it('add a word to the list') do
+    visit('/')
+    fill_in('name', :with => 'Apple')
+    click_button('✓Add a Word!')
+    click_button('Look at a random word')
+    expect(page).to have_content('No Definition added yet')
+  end
+end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,35 +3,44 @@ require('rspec')
 require('pry')
 
 describe('Word') do
-  word = Word.new({"name" => "hat"})
-
+  before() do
+    Word.clear()
+  end
   describe('#initialize') do
     it("saves initializtion values as intance variables that can be read") do
+      word = Word.new({"name" => "hat"})
       expect(word.name).to eq("hat")
     end
   end
 
   describe('#display') do
     it('returns the list of words') do
+      word = Word.new({"name" => "hat"})
+      word.save
       expect(Word.display).to eq([word])
     end
   end
 
   describe('#save') do
-    word.save()
     it('saves the chosen word to a class variable called list') do
+      word = Word.new({"name" => "hat"})
+      word.save
       expect(Word.display[0].name).to eq('hat')
     end
   end
 
   describe('#search') do
     it('searches for the given word') do
+      word = Word.new({"name" => "hat"})
+      word.save
       expect(Word.search('hat').name).to eq('hat')
     end
   end
 
   describe('#remove') do
     it('removes word from list') do
+      word = Word.new({"name" => "hat"})
+      word.save
       Word.remove('hat')
       expect(Word.display).to eq([])
     end
@@ -39,6 +48,7 @@ describe('Word') do
 
   describe('#add_definition') do
     it('adds a definition for a chosen word') do
+      word = Word.new({"name" => "hat"})
       word.save
       Word.add_definition('hat', 'a piece of apparel worn on the head')
       expect(Word.display[0].definitions[0]).to eq ('a piece of apparel worn on the head')
@@ -47,6 +57,8 @@ describe('Word') do
 
   describe('#add_link') do
     it('adds a definition for a chosen word') do
+      word = Word.new({"name" => "hat"})
+      word.save
       Word.add_link('hat', 'https://thelongdark.gamepedia.com/media/thelongdark.gamepedia.com/thumb/4/4b/Basic_Wool_Hat.png/200px-Basic_Wool_Hat.png?version=d4b2a122a5819fdc2a55fcbd4aed0336')
 
       expect(Word.display[0].link).to eq ('https://thelongdark.gamepedia.com/media/thelongdark.gamepedia.com/thumb/4/4b/Basic_Wool_Hat.png/200px-Basic_Wool_Hat.png?version=d4b2a122a5819fdc2a55fcbd4aed0336')
@@ -55,6 +67,8 @@ describe('Word') do
 
   describe('#sort') do
     it('sorts the list of words alphabetically') do
+      word = Word.new({"name" => "hat"})
+      word.save
       word2 = Word.new({'name' => 'acorn'})
       word2.save
       Word.sort
@@ -64,10 +78,10 @@ describe('Word') do
 
   describe('#clear') do
     it("clears all items from the dictionary ") do
+      word = Word.new({"name" => "hat"})
+      word.save
       Word.clear()
       expect(Word.display).to eq([])
     end
   end
-
-
 end
