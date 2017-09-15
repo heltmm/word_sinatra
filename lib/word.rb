88fa4215acf1dfbbf399a8d23f98cdcc93a40757
Mark Helt <heltmm@gmail.com>
2017-09-15 09:20:02 -0700

    add method add_link, add_definition, along with specs to test

--- lib/word.rb
+++ lib/word.rb
@@ -29,5 +29,13 @@ class Word
     @@list.delete(Word.search(name))
   end
 
+  def self.add_definition(name, definition)
+    Word.search(name).definition = definition
+  end
+
+  def self.add_link(name, link)
+      Word.search(name).link = link
+  end
+
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -11,15 +11,16 @@ describe('Word') do
     end
   end
 
+  describe('#display') do
+    it('returns the list of words') do
+      expect(Word.display).to eq([word])
+    end
+  end
+
   describe('#save') do
     word.save()
     it('saves the chosen word to a class variable called list') do
       expect(Word.display[0].name).to eq('hat')
-    end
-  end
-  describe('#display') do
-    it('returns the list of words') do
-      expect(Word.display).to eq([word])
     end
   end
 
@@ -38,16 +39,17 @@ describe('Word') do
 
   describe('#add_definition') do
     it('adds a definition for a chosen word') do
+      word.save
       Word.add_definition('hat', 'a piece of apparel worn on the head')
-      expect(Word.dispaly[0].definition).to eq ('a piece of apparel worn on the head')
+      expect(Word.display[0].definition).to eq ('a piece of apparel worn on the head')
     end
   end
 
   describe('#add_link') do
     it('adds a definition for a chosen word') do
-      Word.add_definition('hat', 'https://thelongdark.gamepedia.com/media/thelongdark.gamepedia.com/thumb/4/4b/Basic_Wool_Hat.png/200px-Basic_Wool_Hat.png?version=d4b2a122a5819fdc2a55fcbd4aed0336')
+      Word.add_link('hat', 'https://thelongdark.gamepedia.com/media/thelongdark.gamepedia.com/thumb/4/4b/Basic_Wool_Hat.png/200px-Basic_Wool_Hat.png?version=d4b2a122a5819fdc2a55fcbd4aed0336')
 
-      expect(@@list[0].link).to eq ('https://thelongdark.gamepedia.com/media/thelongdark.gamepedia.com/thumb/4/4b/Basic_Wool_Hat.png/200px-Basic_Wool_Hat.png?version=d4b2a122a5819fdc2a55fcbd4aed0336')
+      expect(Word.display[0].link).to eq ('https://thelongdark.gamepedia.com/media/thelongdark.gamepedia.com/thumb/4/4b/Basic_Wool_Hat.png/200px-Basic_Wool_Hat.png?version=d4b2a122a5819fdc2a55fcbd4aed0336')
     end
   end
 end
